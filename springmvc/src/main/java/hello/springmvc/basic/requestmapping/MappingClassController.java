d6610a2dfcd9fb6fa11481ba5c89f8445d53844b
Jo YoungHan <verin4494@gmail.com>
2022-09-27 00:13:03 +0900

    springmvc

---
+++ springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
@@ -0,0 +1,36 @@
+package hello.springmvc.basic.requestmapping;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("mapping/users")
+public class MappingClassController {
+
+
+    @GetMapping
+    public String user() {
+        return "get users";
+    }
+
+    @PostMapping
+    public String addUser() {
+        return "post user";
+    }
+
+    @GetMapping("/{userId}")
+    public String findUser(@PathVariable Long userId) {
+        return "get userId = " + userId;
+    }
+
+    @PatchMapping("/{userId}")
+    public String updateUser(@PathVariable Long userId) {
+        return "update userId = " + userId;
+    }
+
+    @DeleteMapping("/{userId}")
+    public String delete(@PathVariable Long userId) {
+        return "delete userId = " + userId;
+    }
+}
