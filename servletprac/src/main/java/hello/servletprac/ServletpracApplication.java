50fa08118235bbe2d3af7f91291919b7b0c63b6f
Jo YoungHan <verin4494@gmail.com>
2022-10-19 23:12:49 +0900

    servlet-prac

---
+++ servletprac/src/main/java/hello/servletprac/ServletpracApplication.java
@@ -0,0 +1,13 @@
+package hello.servletprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServletpracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServletpracApplication.class, args);
+	}
+
+}
---
+++ servletprac/src/test/java/hello/servletprac/ServletpracApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.servletprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServletpracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
